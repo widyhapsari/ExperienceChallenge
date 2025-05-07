c107d44ce1e3e84c92eaef4b16844146f1af08bf
widyhapsari <rastyawidya@gmail.com>
2025-05-07 15:59:12 +0700

    Initial Commit

---
+++ ExperienceChallenge/ExperienceChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperienceChallengeApp.swift
+//  ExperienceChallenge
+//
+//  Created by Rastya Widya Hapsari on 07/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ExperienceChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
