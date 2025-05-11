28f29e4f4ed4f582f1638f9600461e3df0d464c7
widyhapsari <rastyawidya@gmail.com>
2025-05-11 23:41:37 +0700

    BusRoute

--- ExperienceChallenge/Bus/BusRoute.swift
+++ ExperienceChallenge/Bus/BusRoute.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct BusRoute: View {
     var body: some View {
         VStack {
-            BusShelter()
+            BusShelter(shelter:"The Breeze", timeHour: 7, timeMinute: 20, interval: 0, edgeSet: .top, whiteCircleOpacity: 1)
         }
     }
 }
@@ -20,28 +20,35 @@ struct BusRoute: View {
 }
 
 struct BusShelter: View {
+    var shelter: String
+    var timeHour: Int
+    var timeMinute: Int
+    var interval: Int
+    var edgeSet: Edge.Set
+    var whiteCircleOpacity: Double
+    
     var body: some View {
         HStack(spacing: 180) {
             HStack(spacing: 22) {
                 ZStack {
                     Rectangle()
                         .fill(Color.ecTosca)
-                        .padding(.top, 40)
+                        .padding(edgeSet, 40)
                         .frame(width: 20, height: 80)
                     
                     Circle()
                         .stroke(style: StrokeStyle(lineWidth: 6))
                         .foregroundColor(.ecTosca)
-                        .background((Color.white).opacity(1))
+                        .background((Color.white).opacity(whiteCircleOpacity))
                         .frame(width: 14, height: 14)
                     
                 }
                 
-                Text("Intermoda")
+                Text(shelter)
                     .font(.callout)
             }
             
-            Text("11.40")
+            Text("\(timeHour + interval).\(timeMinute)")
                 .font(.callout)
                 .padding(.trailing)
             
