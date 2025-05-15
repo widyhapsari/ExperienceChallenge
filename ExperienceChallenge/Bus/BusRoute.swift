85b161c6d19759d5f11d14d6b0131b66f0ee6c4e
widyhapsari <rastyawidya@gmail.com>
2025-05-15 18:20:13 +0700

    CompleteRouteDetail

--- ExperienceChallenge/Bus/BusRoute.swift
+++ ExperienceChallenge/Bus/BusRoute.swift
@@ -13,10 +13,16 @@ func shelterData(for routeCode: String) -> [ShelterData] {
         return GreenwichPark_Sektor13
     case "BC":
         return TheBreeze_ICE
+    case "ID1":
+        return Intermoda_DePark1
     case "ID2":
         return Intermoda_DePark2
     case "EC":
         return Intermoda_CBD
+    case "IS":
+        return Intermoda_Sektor13
+    case "IV":
+        return Intermoda_VanyaPark
     default:
         return [] // or return a default route if needed
     }
@@ -158,6 +164,101 @@ let Intermoda_CBD: [ShelterData] = [
     .init(shelter: "Simplicity 1", interval: 57),
     .init(shelter: "Terminal Intermoda", interval: 61)
 ]
+
+let Intermoda_DePark1: [ShelterData] = [
+    .init(shelter: "Terminal Intermoda", interval: 0),
+    .init(shelter: "Simplicity 2", interval: 3),
+    .init(shelter: "Halte Edutown | BSD Link", interval: 5),
+    .init(shelter: "Edutown 2", interval: 6),
+    .init(shelter: "ICE 1", interval: 8),
+    .init(shelter: "ICE 2", interval: 10),
+    .init(shelter: "ICE Business Park", interval: 12),
+    .init(shelter: "ICE 6", interval: 13),
+    .init(shelter: "ICE 5", interval: 14),
+    .init(shelter: "Froogy", interval: 16),
+    .init(shelter: "Gramedia", interval: 18),
+    .init(shelter: "Astra", interval: 19),
+    .init(shelter: "Courts Mega Store", interval: 21),
+    .init(shelter: "QBig 1", interval: 22),
+    .init(shelter: "Lulu", interval: 23),
+    .init(shelter: "Greenwich Park 1", interval: 24),
+    .init(shelter: "Greenwich Park Office", interval: 26),
+    .init(shelter: "Jadeite 2", interval: 30),
+    .init(shelter: "De Maja", interval: 33),
+    .init(shelter: "De Heliconia 2", interval: 34),
+    .init(shelter: "De Nara", interval: 35),
+    .init(shelter: "De Park 2", interval: 38),
+    .init(shelter: "Nava Park 2", interval: 40),
+    .init(shelter: "Giardina", interval: 42),
+    .init(shelter: "Collinare", interval: 44),
+    .init(shelter: "Foglio", interval: 46),
+    .init(shelter: "Studento 2", interval: 47),
+    .init(shelter: "Albera", interval: 49),
+    .init(shelter: "Foresta 1", interval: 50),
+    .init(shelter: "GOP 1", interval: 54),
+    .init(shelter: "SML Plaza", interval: 57),
+    .init(shelter: "The Breeze", interval: 60),
+    .init(shelter: "Casa De Parco 2", interval: 63),
+    .init(shelter: "Digital Hub 1", interval: 64),
+    .init(shelter: "Digital Hub 2", interval: 65),
+    .init(shelter: "Verdant View", interval: 67),
+    .init(shelter: "Eternity", interval: 68),
+    .init(shelter: "Terminal Intermoda", interval: 73)
+]
+
+let Intermoda_Sektor13: [ShelterData] = [
+    .init(shelter: "Terminal Intermoda", interval: 0),
+    .init(shelter: "Cosmo", interval: 8),
+    .init(shelter: "Verdant View", interval: 9),
+    .init(shelter: "Eternity", interval: 10),
+    .init(shelter: "Simpicity 2", interval: 12),
+    .init(shelter: "Halte Edutown | BSD Link", interval: 14),
+    .init(shelter: "Edutown 2", interval: 15),
+    .init(shelter: "ICE 1", interval: 17),
+    .init(shelter: "ICE 2", interval: 18),
+    .init(shelter: "ICE Business Park", interval: 20),
+    .init(shelter: "ICE 6", interval: 21),
+    .init(shelter: "ICE 5", interval: 22),
+    .init(shelter: "GOP 1", interval: 29),
+    .init(shelter: "SML Plaza", interval: 31),
+    .init(shelter: "The Breeze", interval: 33),
+    .init(shelter: "CBD Timur 1", interval: 35),
+    .init(shelter: "GOP 2", interval: 36),
+    .init(shelter: "Nava Park 1", interval: 37),
+    .init(shelter: "SWA 2", interval: 39),
+    .init(shelter: "BSD City 1", interval: 41),
+    .init(shelter: "Eka Hospital 1", interval: 43),
+    .init(shelter: "Puspita Loka", interval: 46),
+    .init(shelter: "Polsek Serpong", interval: 49),
+    .init(shelter: "Ruko Madrid", interval: 52),
+    .init(shelter: "Pasar Modern Timur", interval: 55),
+    .init(shelter: "Griyaloka 1", interval: 58),
+    .init(shelter: "Sektor 1.3", interval: 60)
+]
+
+let Intermoda_VanyaPark: [ShelterData] = [
+    .init(shelter: "Terminal Intermoda", interval: 0),
+    .init(shelter: "Simplicity 2", interval: 4),
+    .init(shelter: "Halte Edutown | BSD Link", interval: 6),
+    .init(shelter: "Edutown 2", interval: 7),
+    .init(shelter: "Ice 1", interval: 9),
+    .init(shelter: "Ice 2", interval: 10),
+    .init(shelter: "ICE Business Park", interval: 12),
+    .init(shelter: "Ice 6", interval: 13),
+    .init(shelter: "Prestigia", interval: 15),
+    .init(shelter: "The Mozia 1", interval: 17),
+    .init(shelter: "Plazza Mozia", interval: 18),
+    .init(shelter: "West Park", interval: 25),
+    .init(shelter: "Vanya Park", interval: 26),
+    .init(shelter: "The Mozia 2", interval: 36),
+    .init(shelter: "Illustria", interval: 38),
+    .init(shelter: "Ice 5", interval: 39),
+    .init(shelter: "CBD Barat 1", interval: 42),
+    .init(shelter: "CBD Barat 2", interval: 43),
+    .init(shelter: "Simplicity 1", interval: 45),
+    .init(shelter: "Terminal Intermoda", interval: 52)
+]
+
 
           
           
